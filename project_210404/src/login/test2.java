29a16156f1d6eeb8cd01749f450001b185023cc8
jks5117 <jks5117@naver.com>
2021-04-06 20:24:50 +0900

    ㅁㄴㅇ

---
+++ project_210404/src/login/test2.java
@@ -0,0 +1,12 @@
+package login;
+
+import javax.swing.JLabel;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		JLabel jlb = new JLabel("안녕하세요");
+		System.out.println(jlb.getText());
+	}
+
+}
