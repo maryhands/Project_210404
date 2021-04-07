312d5e42d8d20af475e06a75ea21aa3212ebdd01
jks5117 <jks5117@naver.com>
2021-04-07 19:10:55 +0900

    메일보내기 기능(서버적용 전)/조기설/210407

--- project_210404/src/calendar/EastMessage.java
+++ project_210404/src/calendar/EastMessage.java
@@ -2,18 +2,28 @@ package calendar;
 
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
-import calendar.EastMessage.BackGroundPanel;
 
 public class EastMessage extends JFrame {
 	JLabel jlb_sen = new JLabel("보내는 사람");
@@ -30,6 +40,14 @@ public class EastMessage extends JFrame {
 	
 	String imgPath = "project_210404\\src\\calendar\\";
 	ImageIcon imgIcon = new ImageIcon(imgPath + "blossom.jpg");
+	
+	//보내는 사람
+	String smtpServer = "smtp.naver.com";
+	final String sendId = "";//보내는 사람 네이버 아이디 입력
+	final String sendPass = "";//보내는 사람 네이버 비밀번호 입력
+	String sendEmailAddress = "";//메일입력 ex)jks5117@naver.com
+
+
 
 	class BackGroundPanel extends JPanel { // 배경화면을 위해서 내부에 클래스 지정
 		public void paintComponent(Graphics g) {
@@ -63,10 +81,18 @@ public class EastMessage extends JFrame {
 		jlb_sen.setHorizontalAlignment(JLabel.CENTER);
 		jlb_rec.setHorizontalAlignment(JLabel.CENTER);
 		jlb_title.setHorizontalAlignment(JLabel.CENTER);
+		jtf_sen_mail.setEditable(false);
+		jtf_sen_mail.setText(sendEmailAddress);
+		
+		send.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sendMail();
+			}
+			
+		});
 		this.add(jsp);
 		this.add(send);
 		this.add(jlb_sen);
-		jtf_sen_mail.setEditable(false);
 		this.add(jlb_rec);
 		this.add(jlb_title);
 		this.add(jtf_sen_mail);
@@ -78,5 +104,47 @@ public class EastMessage extends JFrame {
 		this.setSize(600, 600);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
+	}
+	public void sendMail() {
+		int smtpPort=465;
+		String receiveEmailAddress = jtf_rec_mail.getText();
+		
+		String subject = jtf_title.getText();
+		//내용
+		String content = jta.getText();
+		
+		
+		Properties props = System.getProperties();
+		props.put("mail.smtp.host", smtpServer);
+		props.put("mail.smtp.port", smtpPort);
+		props.put("mail.smtp.auth", true);
+		props.put("mail.smtp.ssl.enable", true);
+		props.put("mail.smtp.ssl.trust", smtpServer);
+		
+		Session session5 = Session.getDefaultInstance(props, new Authenticator(){
+			protected PasswordAuthentication getPasswordAuthentication(){
+				return new PasswordAuthentication(sendId, sendPass);
+			}
+		});
+		//session2.setDebug(true);
+		try{
+			Message mimeMessage = new MimeMessage(session5);
+			mimeMessage.setFrom(new InternetAddress(sendEmailAddress));
+			mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(receiveEmailAddress));
+			mimeMessage.setSubject(subject);
+			mimeMessage.setText(content);
+			Transport.send(mimeMessage);
+			System.out.print("message sent successfully..."); 
+			JOptionPane.showMessageDialog(null, "메일 전송에 성공했습니다.");
+		} catch (AddressException e) { 
+			// TODO Auto-generated catch block
+			JOptionPane.showMessageDialog(null, "메일 전송에 실패했습니다.");
+			e.printStackTrace(); 
+		} catch (MessagingException e) { 
+			// TODO Auto-generated catch block 
+			e.printStackTrace(); 
+			JOptionPane.showMessageDialog(null, "메일 전송에 실패했습니다.");
+		} 			
+	
 	}
 }
